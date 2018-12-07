7d5ee6f24cf55cdab51aa2d3d2c45456ddd334df
iamdotasb <lucifergong@gmail.com>
2018-12-07 17:42:57 +0800

    Same as the last commit

--- led_scan_t.v
+++ led_scan_t.v
@@ -4,4 +4,5 @@
 		    if(hs_dly_cnt == `OE_L_DLY + 1)
 
 wire oe_line;
-assign oe_line = (led_line_dly > oe_cnt[frame_cnt]*`LINE_INT/16)? 1'b0: 1'b1;
+assign oe_line = (led_line_dly != `LINE_INT &&
+    led_line_dly > oe_cnt[frame_cnt]*`LINE_INT/16)? 1'b0: 1'b1;
